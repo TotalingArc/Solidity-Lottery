1d86e8a18e95e3e70bbaff556f99be77e1e424eb
Totaling Arc <78669633+TotalingArc@users.noreply.github.com>
2025-04-10 14:18:37 -0700

    Update fund-contract.js

--- scripts/fund-contract.js
+++ scripts/fund-contract.js
@@ -2,7 +2,7 @@ const MyContract = artifacts.require('MyContract')
 const LinkTokenInterface = artifacts.require('LinkTokenInterface')
 
 /*
-  This script is meant to assist with funding the requesting
+  This script is meant to assist funding the requesting
   contract with LINK. It will send (1) LINK to the requesting
   contract for ease-of-use. Any extra LINK present on the contract
   can be retrieved by calling  withdrawLink() function.
